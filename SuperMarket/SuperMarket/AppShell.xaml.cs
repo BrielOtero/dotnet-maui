77c5054e242ab244fb2c898503a08bbeb309b615
Gabriel O <info.otero.gabriel@gmail.com>
2022-12-08 19:07:42 +0100

    Save

--- SuperMarket/SuperMarket/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SuperMarket;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- SuperMarket/SuperMarket/Views/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace SuperMarket.Views;
-
-public partial class MainPage : ContentPage
-{
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-}
